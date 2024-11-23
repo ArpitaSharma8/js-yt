6f834f70910b82b5e761ff85572760ed65bc629c
Arpita Sharma <147055526+ArpitaSharma8@users.noreply.github.com>
2024-11-23 06:25:30 +0000

    changes

---
+++ 01_basics/dataTypes-summery.js
@@ -0,0 +1,37 @@
+// Premitive
+
+// 7 types: String , Number , Bollean , null , undefined , Symbol , BigInt 
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+         
+
+// const bigNumber = 3457345035370289348n
+
+// Reference (Non Premitive) 
+
+// Array, Objects, Functions
+
+// const heros = ["Shaktiman" , "NaagRaj" , "doga"];
+// let myObj = {
+//     name: "Arpita",
+//     age: 19,
+// }
+
+const myfunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myfunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
